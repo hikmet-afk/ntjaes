a30b98d6d9b7462134915277ade36fd0b9214ccd
hikmet-afk <80512617+hikmet-afk@users.noreply.github.com>
2021-03-26 19:17:46 +0300

    Add files via upload

---
+++ why-moderation-2021-03-26_160317/app/bot.js
@@ -0,0 +1,251 @@
+const discord = require("discord.js");
+const fs = require("fs");
+const http = require("http");
+const db = require("quick.db");
+const moment = require("moment");
+const express = require("express");
+const ayarlar = require("./ayarlar.json");
+const app = express();
+app.get("/", (request, response) => {
+  console.log(`7/24 Hizmet Vermekteyim!`);
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
+
+//READY.JS
+
+const Discord = require("discord.js");
+const client = new Discord.Client();
+client.on("ready", async () => {
+  client.appInfo = await client.fetchApplication();
+  setInterval(async () => {
+    client.appInfo = await client.fetchApplication();
+  }, 600);
+
+  client.user.setActivity(`Why 💖`, { type: "PLAYING" });
+
+  console.log("Rolly Code #AFFETMEZ!!");
+});
+
+const log = message => {
+  console.log(` ${message}`);
+};
+require("./util/eventLoader.js")(client);
+
+//READY.JS SON
+
+//KOMUT ALGILAYICI
+
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+fs.readdir("./komutlar/", (err, files) => {
+  if (err) console.error(err);
+  log(`${files.length} komut yüklenecek.`);
+  files.forEach(f => {
+    let props = require(`./komutlar/${f}`);
+    log(`Yüklenen komut: ${props.help.name}.`);
+    client.commands.set(props.help.name, props);
+    props.conf.aliases.forEach(alias => {
+      client.aliases.set(alias, props.help.name);
+    });
+  });
+});
+
+client.reload = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      delete require.cache[require.resolve(`./komutlar/${command}`)];
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.delete(command);
+      client.aliases.forEach((cmd, alias) => {
+        if (cmd === command) client.aliases.delete(alias);
+      });
+      client.commands.set(command, cmd);
+      cmd.conf.aliases.forEach(alias => {
+        client.aliases.set(alias, cmd.help.name);
+      });
+      resolve();
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+client.load = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.set(command, cmd);
+      cmd.conf.aliases.forEach(alias => {
+        client.aliases.set(alias, cmd.help.name);
+      });
+      resolve();
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+client.unload = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      delete require.cache[require.resolve(`./komutlar/${command}`)];
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.delete(command);
+      client.aliases.forEach((cmd, alias) => {
+        if (cmd === command) client.aliases.delete(alias);
+      });
+      resolve();
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+//KOMUT ALGILAYICI SON
+
+client.elevation = message => {
+  if (!message.guild) {
+    return;
+  }
+  let permlvl = 0;
+  if (message.member.hasPermission("BAN_MEMBERS")) permlvl = 2;
+  if (message.member.hasPermission("ADMINISTRATOR")) permlvl = 3;
+  if (message.author.id === ayarlar.sahip) permlvl = 4;
+  return permlvl;
+};
+client.login(process.env.TOKEN);
+
+//-----------------------KOMUTLAR-----------------------\\
+//token .env
+//ready.js mainde :)
+
+//sa as
+client.on("message", async message => {
+  let a = await db.fetch(`saas_${message.guild.id}`)
+  if (a) {
+      if (message.content.toLowerCase() === "sa") {
+        message.channel.send(
+new Discord.MessageEmbed()
+          .setDescription(`Aleyküm Selam Hoşgeldin!`)
+)
+      }
+  }
+  
+  
+})
+
+//ROL KORuma
+client.on("roleDelete", async role => {
+  let rolko = await db.fetch(`rolk_${role.guild.id}`);
+  if (rolko) { 
+         const entry = await role.guild.fetchAuditLogs({ type: "ROLE_DELETE" }).then(audit => audit.entries.first());
+    if (entry.executor.id == client.user.id) return;
+  role.guild.roles.create({ data: {
+          name: role.name,
+          color: role.color,
+          hoist: role.hoist,
+          permissions: role.permissions,
+          mentionable: role.mentionable,
+          position: role.position
+}, reason: 'Silinen Roller Tekrar Açıldı.'})
+  }
+})
+
+//
+
+client.on("roleCreate", async role => {
+  let rolk = await db.fetch(`rolk_${role.guild.id}`);
+  if (rolk) { 
+       const entry = await role.guild.fetchAuditLogs({ type: "ROLE_CREATE" }).then(audit => audit.entries.first());
+    if (entry.executor.id == client.user.id) return;
+  role.delete()
+}
+})
+
+//////////reklamengel
+client.on('message', async message => {
+  
+let aktif = await db.fetch(`reklamEngelcodework_${message.channel.id}`)
+if (!aktif) return 
+  
+let reklamlar = ["discord.app", "discord.gg" ,"discordapp","discordgg", ".com", ".net", ".xyz", ".tk", ".pw", ".io", ".me", ".gg", "www.", "https", "http", ".gl", ".org", ".com.tr", ".biz", ".party", ".rf.gd", ".az", ".cf", ".me", ".in"]
+let kelimeler = message.content.slice(" ").split(/ +/g)
+
+if (reklamlar.some(word => message.content.toLowerCase().includes(word))) {
+  
+if (message.member.hasPermission("BAN_MEMBERS")) return; message.delete()
+  
+message.reply('Reklamları engelliyorum!').then(msg => msg.delete(7000)) 
+}
+});
+
+client.on("messageUpdate", async (oldMsg, newMsg) => {
+  
+let aktif = await db.fetch(`reklamEngelcodework_${oldMsg.channel.id}`)
+if(!aktif) return
+  
+let reklamlar = ["discord.app", "discord.gg","discordapp","discordgg", ".com", ".net", ".xyz", ".tk", ".pw", ".io", ".me", ".gg", "www.", "https", "http", ".gl", ".org", ".com.tr", ".biz", ".party", ".rf.gd", ".az", ".cf", ".me", ".in"]
+let kelimeler = newMsg.content.slice(" ").split(/ +/g)
+
+if (reklamlar.some(word => newMsg.content.toLowerCase().includes(word))) {
+  
+if (newMsg.member.hasPermission("BAN_MEMBERS")) return; newMsg.delete()
+  
+oldMsg.reply('Reklamları engelliyorum!').then(msg => msg.delete(7000)) 
+}
+});
+/////////////////////////////////////////////////kanal-koruma
+//KanalKoruma
+client.on("channelDelete", async function(channel) {
+    let rol = await db.fetch(`kanalk_${channel.guild.id}`);
+  
+  if (rol) {
+const guild = channel.guild.cache;
+let channelp = channel.parentID;
+
+  channel.clone().then(z => {
+    let kanal = z.guild.channels.find(c => c.name === z.name);
+    kanal.setParent(
+      kanal.guild.channels.find(channel => channel.id === channelp)
+      
+    );
+  });
+  }
+})
+
+client.on("ready", () => {
+        client.channels.cache.get("818409654287597599").join();
+      })
+
+// tag rol
+
+client.on("userUpdate", async (stg, yeni) => {
+  var sunucu = client.guilds.cache.get('815971195647819816'); // Buraya Sunucu ID
+  var uye = sunucu.members.cache.get(yeni.id);
+  var ekipTag = "て"; // Buraya Ekip Tag
+  var ekipRolü = "815971195685830658"; // Buraya Ekip Rolünün ID
+  var logKanali = "815971199313510406"; // Loglanacağı Kanalın ID
+
+  if (!sunucu.members.cache.has(yeni.id) || yeni.bot || stg.username === yeni.username) return;
+  
+  if ((yeni.username).includes(ekipTag) && !uye.roles.cache.has(ekipRolü)) {
+    try {
+      await uye.roles.add(ekipRolü);
+      await uye.send(`Tagımızı aldığın için teşekkürler! Aramıza hoş geldin. İyi Vakitler Geçirmen Dileğiyle.`);
+      await client.channels.cache.get(logKanali).send(new Discord.MessageEmbed().setColor('GREEN').setDescription(`${yeni} adlı üye tagımızı alarak aramıza katıldı!`));
+    } catch (err) { console.error(err) };
+  };
+  
+  if (!(yeni.username).includes(ekipTag) && uye.roles.cache.has(ekipRolü)) {
+    try {
+      await uye.roles.remove(uye.roles.cache.filter(rol => rol.position >= sunucu.roles.cache.get(ekipRolü).position));
+      await uye.send(`Tagımızı bıraktığın için ekip rolü ve yetkili rollerin alındı! Tagımızı tekrar alıp aramıza katılmak istersen;\nTagımız: **${ekipTag}**`);
+      await client.channels.cache.get(logKanali).send(new Discord.MessageEmbed().setColor('RED').setDescription(`${yeni} adlı üye tagımızı bırakarak aramızdan ayrıldı!`));
+    } catch(err) { console.error(err) };
+  };
+});
+
